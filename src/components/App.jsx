043e214f4ef32c943f5684a10beb125b425fecfd
Sonia <sonia.wangari@student.moringaschool.com>
2026-05-08 11:02:05 +0300

    Implement full CRUD for Toy Tales

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,54 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  // 1. SEE ALL TOYS (GET)
+  useEffect(() => {
+    fetch("http://localhost:6001/toys")
+      .then((r) => r.json())
+      .then((data) => setToys(data));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  // 2. CREATE NEW TOY (POST)
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy]);
+  }
+
+  // 3. INCREASE LIKES (PATCH)
+  function handleUpdateToy(updatedToy) {
+    const updatedToys = toys.map((toy) =>
+      toy.id === updatedToy.id ? updatedToy : toy
+    );
+    setToys(updatedToys);
+  }
+
+  // 4. DELETE TOY (DELETE)
+  function handleDeleteToy(deletedToy) {
+    const updatedToys = toys.filter((toy) => toy.id !== deletedToy.id);
+    setToys(updatedToys);
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer 
+        toys={toys} 
+        onDeleteToy={handleDeleteToy} 
+        onUpdateToy={handleUpdateToy} 
+      />
     </>
   );
 }
